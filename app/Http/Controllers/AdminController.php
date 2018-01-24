5886ce4fde2bd1d7041bbd5c2d17aa167ab73944
jovanna <ladybutterfly.fly@gmail.com>
2018-01-24 19:47:49 +0100

    added admin guard

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // without :admin it would call default guard which is user guard
+        // we want to prevent user to be able to see admin dashboard so add :admin guard
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin');
+    }
+}
